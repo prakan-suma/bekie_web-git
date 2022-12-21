55875d12c2fbafcecec2b30bb796237fc5c8cffb
@_D!vhed <55022692+prakan-suma@users.noreply.github.com>
2022-12-21 09:06:55 +0700

    Update and rename shop_edit.php to customer_edit.php

--- customer_edit.php
+++ customer_edit.php
@@ -61,11 +61,5 @@ if (!isset($_SESSION['login'])) {
 	</form>
 
 </div>
-<script src="js/jquery-3.3.1.min.js"></script>
 
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap-4.3.1.js"></script>
-</body>
-
-</html>
+<?php include'footer.php' ?>
